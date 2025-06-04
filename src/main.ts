4f94097f61a27adb5f7286e51ef715f871b5ef8b
Jimver <jimver1@hotmail.com>
2025-06-04 20:19:13 +0000

    Create test.txt file and glob it

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as glob from '@actions/glob'
+import fs from 'fs'
 
 /**
  * The main function for the action.
@@ -7,21 +8,18 @@ import { wait } from './wait.js'
  * @returns Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+  // Create a file in the current working directory using fs.promises
+  const filePath = `test.txt`
+  core.debug(`Creating file at ${filePath}`)
+  fs.promises.writeFile(filePath, `test content`)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  // List files in current directory using fs.promises
+  const filesInCurrentDirectory = await fs.promises.readdir(`.`)
+  core.debug(`Files in current directory: ${filesInCurrentDirectory}`)
+  // List files in current directory using glob
+  const globber = await glob.create(`*`)
+  const filesInCurrentDirectoryGlob = await globber.glob()
+  core.debug(
+    `Files in current directory (glob): ${filesInCurrentDirectoryGlob}`
+  )
 }
